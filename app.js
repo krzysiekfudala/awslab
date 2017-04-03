1b04319a6a1ed10bd4181bd4ed7bae6ef7960abb
KF <krzysztof.fudala@jmk.pl>
2017-04-03 12:32:37 +0200

    added lab2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
+var lab2 = require ("./lab/lab2").lab;
 
 var PORT = 8080;
 
@@ -8,6 +9,7 @@ var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
+	{path: "/lab2", action: lab2},
 	];
 
 var service = require("./lib/service").http(urlMap);
