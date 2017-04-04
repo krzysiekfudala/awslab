f57263f50d29a694f41ff171fe061830e6fb98ef
KF <krzysztof.fudala@jmk.pl>
2017-04-04 09:42:31 +0200

    walka

--- lab/lab2.js
+++ lab/lab2.js
@@ -1,11 +1,20 @@
 //stub for lab 1_2
 var AWS = require('aws-sdk');
-
 AWS.config.loadFromPath('./config.json');
+var ec2 = new AWS.EC2();
+var params = {};
 
 var task =  function(request, callback){
-	
-	
-}
 
+		ec2.describeInstances(params, function(err, data) {
+			  if (err) {
+			  		console.log(err, err.stack);
+			  		return callback(err);
+			  } // an error occurred
+			  else {
+			  	console.log(data);  
+			  	return callback(null,data);
+			  }         // successful response	
+		});
+};
 exports.lab = task
